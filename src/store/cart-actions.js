66ffdfd98864b95ddd034c1585f3f900ec9ad495
Andrews <allwinandrews@gmail.com>
2021-07-06 15:11:46 +0530

    Uses Thunk logic in cart-actions to fetch cartData

---
+++ src/store/cart-actions.js
@@ -0,0 +1,78 @@
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const url = "https://react-http-default-rtdb.firebaseio.com/cart.json";
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Could not fetch data");
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Fetching Cart Data failed",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending Cart Data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const url =
+        "https://react-http-44a66-default-rtdb.firebaseio.com/cart.json";
+
+      const response = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("SENDING CART DATA FAILED");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent Cart Data successfully",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending Cart Data failed",
+        })
+      );
+    }
+  };
+};
